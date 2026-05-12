8aff50a961ac026d9cb23acc4fe2da2e70e1a603
Occult09 <maheermusfiq221@gmail.com>
2026-05-12 16:50:21 +0600

    Parsing request body

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (req: IncomingMessage, res: ServerResponse) => {
+export const productController = async(req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
 
@@ -29,6 +30,14 @@ export const productController = (req: IncomingMessage, res: ServerResponse) => 
         res.end(JSON.stringify({
             message : `This is product ${id}`,
             data: product
+        }))
+    } 
+    else if(method === 'POST' && url === 'products'){
+        const body = await parseBody(req);
+        console.log('Body', body);
+        res.writeHead(200, {'content-type' : 'application/json'})
+        res.end(JSON.stringify({
+            message: 'This is products route',
         }))
     }
 };
